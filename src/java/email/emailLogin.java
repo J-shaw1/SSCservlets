dfbb1f4a172116d0a232e0e6d86f6833d27c9435
J-shaw1 <joeramsfan@hotmail.co.uk>
2014-12-08 08:13:35 +0000

    Adding files

---
+++ src/java/email/emailLogin.java
@@ -0,0 +1,55 @@
+package email;
+
+import java.util.Properties;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+
+/**
+ * Contains several methods for interacting with an email account
+ *
+ * @author Joe
+ */
+public class EmailLogin {
+
+    String username;
+    String password;
+    Session s;
+
+    /**
+     * Create the object with the username and password
+     *
+     * @param username The username to use in the object
+     * @param password The password to use in the object
+     */
+    public EmailLogin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Tests whether the given login details are valid for accessing and email
+     * account
+     *
+     * @return True if the details are valid, false if they are not valid.
+     */
+    public boolean testLoginDetails() {
+
+        //Set all properties needed
+        Properties prop = System.getProperties();
+        prop.setProperty("mail.store.protocol", "imaps");
+        prop.setProperty("mail.user", username);
+        prop.setProperty("mail.password", password);
+
+        //Create a session
+        s = Session.getDefaultInstance(prop);
+
+        try {
+            //If we can get the store, then the login details must be valid
+            s.getStore("imaps").connect("imap.googlemail.com", username, password);
+            return true;
+        } catch (MessagingException e) {
+            //Something went wrong, so the details must be invalid, or the connection failed
+            return false;
+        }
+    }
+}
---
+++ src/java/servlets/TestLogin.java
@@ -0,0 +1,89 @@
+package servlets;
+
+import email.EmailLogin;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ *
+ * @author Joe
+ */
+public class TestLogin extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        //Test if the deatils are correct
+            //IF they are then send to menu page
+            //else send to try again page
+        
+        RequestDispatcher rd;
+        
+        //Get the usename and password from the request
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        //Create the new login object
+        EmailLogin login = new EmailLogin(username, password);
+        
+        //Add the login to the request
+        request.setAttribute("login", login);
+        
+        //Test if the login details are correct
+        if(login.testLoginDetails()){
+            rd = request.getRequestDispatcher("correctLogin.jsp");
+        } else {
+            rd = request.getRequestDispatcher("incorrectLogin.html");
+        }
+        rd.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
